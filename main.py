6fce36018da0797f8644ad24c7cc7e615820964f
Guilherme Henrique <guihenrique17020@uin9.edu.br>
2025-07-11 20:05:05 -0300

    Inserção de caixa de entrada para usuário/ inicio de arvore de escolhas/ criação da primeira função de adição de tarefa

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 Tarefas = [] 
 
+def adicionarTarefa():
+    adicao = input('Descreva sua tarefa:  ')
+    Tarefas.append(adicao)
+
 print('1- Adicione uma nova tarefa')
 print('2- Listar Todas as tarefas')
 print('3- Encerrar')
+escolha = int(input(' Escolha uma opção acima: '))
+
+
+if escolha == 1: 
+    print('Adicionando nova tarefa....')
+    adicionarTarefa()
