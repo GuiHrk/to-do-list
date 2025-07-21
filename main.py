152294249d56e19dc7cdb262328967a3176e7979
Guilherme Henrique <guihenrique17020@uin9.edu.br>
2025-07-21 20:57:16 -0300

    modificação: adicionado loop para rodar programa até escolha de sair// Adição de função de listagem de Tarefas em Lista

--- main.py
+++ main.py
@@ -4,16 +4,25 @@ def adicionarTarefa():
     adicao = input('Descreva sua tarefa:  ')
     Tarefas.append(adicao)
 
-print('1- Adicione uma nova tarefa')
-print('2- Listar Todas as tarefas')
-print('3- Encerrar')
-escolha = int(input(' Escolha uma opção acima: '))
+def Listar_tarefas():
+    print(Tarefas)
+
+while True: 
+    print('1- Adicione uma nova tarefa')
+    print('2- Listar Todas as tarefas')
+    print('3- Encerrar')
+    escolha = int(input(' Escolha uma opção acima: '))
 
 
-if escolha == 1: 
-    print('Adicionando nova tarefa....')
-    adicionarTarefa()
-elif escolha == 2:
-    print('Listando Todas as tarefas.....')
-else:
-    print('Encerrando o programa.....')
+
+    if escolha == 1: 
+        print('Adicionando nova tarefa....')
+        adicionarTarefa()     
+    elif escolha == 2:
+        print('Listando Todas as tarefas.....')
+        Listar_tarefas()
+    elif escolha == 3:
+        print('Encerrando o programa.....')
+        break
+    else:
+        print('opção inválida')
