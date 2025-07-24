78e86c75bfc4bfb354246f9f0d4651b10313ca6e
Guilherme Henrique <guihenrique17020@uin9.edu.br>
2025-07-24 16:13:24 -0300

    Organização visual da listagem de tarefas

--- main.py
+++ main.py
@@ -5,15 +5,20 @@ def adicionarTarefa():
     Tarefas.append(adicao)
 
 def Listar_tarefas():
-    print(Tarefas)
+   # print(Tarefas)
+    for lista, value in enumerate(Tarefas, start=1):
+        print(f"{lista} --- Tarefa: {value}")
 
+# proximo passo organização visual da listagem
+# Adicionar opção de remover tarefas
+# Adicionar status de tarefa
 while True: 
     print('1- Adicione uma nova tarefa')
+    print()
     print('2- Listar Todas as tarefas')
+    print()
     print('3- Encerrar')
     escolha = int(input(' Escolha uma opção acima: '))
-
-
 
     if escolha == 1: 
         print('Adicionando nova tarefa....')
