688138f5759c9cfb45a5e43fb4b0b518eb849805
Guilherme Henrique <guihenrique17020@uin9.edu.br>
2025-07-25 19:52:33 -0300

    Implementação de função de verificação de Lista Vazia

--- main.py
+++ main.py
@@ -5,13 +5,12 @@ def adicionarTarefa():
     Tarefas.append(adicao)
 
 def Listar_tarefas():
-   # print(Tarefas)
-    for lista, value in enumerate(Tarefas, start=1):
+    if not Tarefas:
+        print('Lista de tarefas vazia!')
+    else: 
+       for lista, value in enumerate(Tarefas, start=1):
         print(f"{lista} --- Tarefa: {value}")
 
-# proximo passo organização visual da listagem
-# Adicionar opção de remover tarefas
-# Adicionar status de tarefa
 while True: 
     print('1- Adicione uma nova tarefa')
     print()
@@ -22,7 +21,8 @@ while True:
 
     if escolha == 1: 
         print('Adicionando nova tarefa....')
-        adicionarTarefa()     
+        adicionarTarefa()
+        print('Tarefa Adicionada com Sucesso!!!')     
     elif escolha == 2:
         print('Listando Todas as tarefas.....')
         Listar_tarefas()
