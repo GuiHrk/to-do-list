b2eda96da683778f72e85b9a1fc5308807e8d95c
Guilherme Henrique <guihenrique17020@uin9.edu.br>
2025-07-30 21:50:53 -0300

    inserção de função de excluir tarefas

--- main.py
+++ main.py
@@ -11,12 +11,18 @@ def Listar_tarefas():
        for lista, value in enumerate(Tarefas, start=1):
         print(f"{lista} --- Tarefa: {value}")
 
+def Excluir_tarefa():
+    lista = int(input('Escolha a tarefa para excluir: ')) -1
+    del Tarefas[lista]
+
 while True: 
     print('1- Adicione uma nova tarefa')
     print()
     print('2- Listar Todas as tarefas')
     print()
-    print('3- Encerrar')
+    print('3- Excluir Tarefa')
+    print()
+    print('4- Encerrar')
     escolha = int(input(' Escolha uma opção acima: '))
 
     if escolha == 1: 
@@ -27,6 +33,9 @@ while True:
         print('Listando Todas as tarefas.....')
         Listar_tarefas()
     elif escolha == 3:
+        print('Excluindo Tarefa selecionada')
+        Excluir_tarefa()
+    elif escolha == 4:
         print('Encerrando o programa.....')
         break
     else:
